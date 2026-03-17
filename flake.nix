52a82d9f1baf7c94501cfe92e8117358d5195f50
Doug Beardsley <mightybyte@gmail.com>
2026-03-16 20:18:30 -0400

    Bump GHC

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
           pureclaw-project =
             final.haskell-nix.cabalProject' {
               src = ./.;
-              compiler-nix-name = "ghc9121";
+              compiler-nix-name = "ghc9123";
               modules = [
                   {
                       enableProfiling = true;
